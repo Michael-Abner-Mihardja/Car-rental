742c2f0984a1afe60d147d454a4537f01e51a2c2
Michael-Abner-Mihardja <michael.mihardja@binus.ac.id>
2025-09-23 16:07:45 +0700

    Add files via upload

---
+++ Car rental.cpp
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+int main() {
+	
+	FILE *fptr;
+	fptr = fopen("Car_rental.txt", "a");
+	
+	switch (option) {
+		case 1: //list car
+			listCar();
+			break;
+		case 2: //rent car
+			break;
+		case 3: //return car
+			break;
+		case 4: //quit program
+			
+			break;
+	}
+	
+	return(0);
+}
+
+
