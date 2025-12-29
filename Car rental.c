673c3fb9a855d1839a44343eab29f72f5fb94bba
Aelexeii-Mikael <aelexeii.tatuwey@binus.ac.id>
2025-12-29 08:28:04 +0700

    Refactor car rental program structure and menu

--- Car rental.c
+++ Car rental.c
@@ -2,42 +2,65 @@
 #include<stdlib.h>
 #include<string.h>
 
-int main() {
-	//declare file pointer
-	FILE *fptr;
-	fptr = fopen("Car_rental.txt", "w"); //open file in writing mode ("w")
-	fprintf//list car
+int main (){
+	main menu ();
+return 0;
+}
 
-	int option;
+void main menu (){
+	int choice;
+do {
+clearScreen();
+
+printf("===== CAR RENT =====\n");
+printf("1. login\n");
+printf("2. rent a car\n");
+printf("3. return a car\n");
+printf("4. exit\n");
+printf("====================\n");
+printf("enter your choice: ");
+
+
+
+
+
+
+// int main() {
+// 	//declare file pointer
+// 	FILE *fptr;
+// 	fptr = fopen("Car_rental.txt", "w"); //open file in writing mode ("w")
+// 	fprintf//list car
+
+// 	int option;
 	
 		
-	do {
-		listAction()
-		switch (option) { //
-		case 1: //list car
+// 	do {
+// 		listAction()
+// 		switch (option) { //
+// 		case 1: //list car
 			
-			break;
-		case 2: //rent car
+// 			break;
+// 		case 2: //rent car
 			
-			break;
-		case 3: //return car
+// 			break;
+// 		case 3: //return car
 			
-			break;
-		case 4: //quit program
-			printf("Exiting\n")
-			break;
-		}
-	}
-		while (option != 4);
-	return(0);
-}
+// 			break;
+// 		case 4: //quit program
+// 			printf("Exiting\n")
+// 			break;
+// 		}
+// 	}
+// 		while (option != 4);
+// 	return(0);
+// }
 
-void listCar() {
-	fptr = fopen("Car_rental.txt", "r");//read file
-	char listCar[1000];
-	fgets(listCar, 1000, fptr);
-	printf("%s", listCar);
-}
+// void listCar() {
+// 	fptr = fopen("Car_rental.txt", "r");//read file
+// 	char listCar[1000];
+// 	fgets(listCar, 1000, fptr);
+// 	printf("%s", listCar);
+// }
 
 
 
@@ -57,3 +80,4 @@ void listCar() {
      
 //     printf("which car would you like to rent?");
 //     scanf()
+
